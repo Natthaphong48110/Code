3d53b1de67bcd0e1202ea079938e3d232da70f0d
Natthaphong48110 <nuttaphong967@gmail.com>
2026-07-02 23:25:22 +0700

    fix

--- calculator.c
+++ calculator.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 int main() {
     //variable
-    char operator;
-    double num1 , num2 , result;
+    char name[20],operator;
+    double num1,num2,result;
 
     //input
-    printf("Enter Your Operator (= , - , * , /) ");
-    scanf("%c",&operator);
-    printf("Enter Your Num1 ");
+    printf("Enter Your Name : ");
+    scanf("%s",&name);
+    printf("Enter Your Operator (+ , - , * , /) : ");
+    scanf(" %c",&operator);
+    printf("Enter Your Num1 : ");
     scanf("%lf",&num1);
-    printf("Enter Your Num2 ");
+    printf("Enter Your Num2 : ");
     scanf("%lf",&num2);
 
     //output
-    switch (operator){
+    printf("Hello %s\n",name);
+    switch (operator) {
         case '+' :
             result = num1 + num2;
             printf("Result is %.2lf + %.2lf = %.2lf",num1,num2,result);
@@ -27,15 +30,19 @@ int main() {
             printf("Result is %.2lf X %.2lf = %.2lf",num1,num2,result);
         break;
         case '/' :
-        if (num2 == 0) {
-            printf("You can't divide by 0");
-        } else {
-            result = num1 / num2;
-            printf("Result is %.2lf / %.2lf = %.2lf",num1,num2,result);
+            if (num2 == 0) {
+                printf("Your can't divide By 0 !!");
+            } else {
+                result = num1 / num2;
+                printf("Result is %.2lf / %.2lf = %.2lf",num1,num2,result);
+            }
         break;
+        default : {
+            printf("Error!!");
         }
-        default : 
-            printf(" Error");
     }
+
     return 0;
+
+
 }
